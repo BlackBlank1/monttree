0e4be31d0047c5322277e9252ad8707ece1167a3
BlackBlank1 <121706904+BlackBlank1@users.noreply.github.com>
2023-02-10 16:41:01 +0800

    111

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,18 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000', //接口域名,端口看各自设置的
+        changeOrigin: true,             //是否跨域
+        ws: true,                       //是否代理 websockets
+        secure: true,                   //是否https接口
+        pathRewrite: {                  //路径重置
+          '^/api': ''
+        }
+      }
+    }
   }
 })
